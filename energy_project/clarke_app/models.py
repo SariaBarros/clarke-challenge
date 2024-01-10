ce7e6910fcdcb883ed9f1066730589a469112e0e
SariaBarros <sandy.vitoriabarros@gmail.com>
2024-01-10 11:56:20 -0400

    fix:ajustando migração

--- energy_project/clarke_app/models.py
+++ energy_project/clarke_app/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 class Fornecedor(models.Model):
     nome = models.CharField(max_length=100)
-    logo = models.ImageField(upload_to='logos/')
-    estado_origem = models.CharField(max_length=50)
-    custo_por_kwh = models.DecimalField(max_digits=5, decimal_places=2)
+    logo = models.ImageField(upload_to='logos/', default=None)
+    estado_origem = models.CharField(max_length=50, default=None)
+    custo_por_kwh = models.DecimalField(max_digits=5, decimal_places=2, default=None)
     limite_minimo_kwh = models.PositiveIntegerField()
     numero_total_clientes = models.PositiveIntegerField()
     avaliacao_media_clientes = models.DecimalField(max_digits=3, decimal_places=2)
